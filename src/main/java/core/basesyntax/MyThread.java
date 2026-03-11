0281b720b99406215dade157650e2fc02cdb1b45
Andrii Honchar <andriihoncharw@gmail.com>
2026-03-11 11:14:54 +0200

    Solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,21 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MyThread implements Callable<String> {
+    private static final Logger log = LogManager.getLogger(MyThread.class);
+
     @Override
-    public String call() throws InterruptedException {
-        // write your code here
-        return null;
+    public String call() {
+        long duration = ThreadLocalRandom.current().nextLong(0L, 500L);
+        LocalTime time = LocalTime.now();
+
+        log.info("Task duration was {} ms, execution finished at {}", duration, time);
+
+        return String.format("Task duration was %s ms, execution finished at %s%n", duration, time);
     }
 }
